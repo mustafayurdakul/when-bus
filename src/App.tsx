a55d4822e61cb62f959eb59eb718b20ca0f97346
Mustafa Yurdakul <mustafa@yurdakul.dev>
2023-07-01 14:49:48 +0300

    O god. Merge.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import React, { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 import AppService from "./App.service";
-import BusCard from "./BusCard";
 import BusInfo from "./types/BusInfo";
 import BusStationInfo from "./types/BusStationInfo";
 
 
 const App: React.FC = () => {
+
+	useEffect(() => {
+		const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+		const themeColorMeta = document.getElementById("theme-color-meta") as HTMLMetaElement;
+
+		if (prefersDarkMode) {
+			document.documentElement.classList.add("dark");
+			if (themeColorMeta) {
+				themeColorMeta.content = "#0a0a0a";
+			}
+		} else {
+			document.documentElement.classList.remove("dark");
+			if (themeColorMeta) {
+				themeColorMeta.content = "rgb(245 245 245)";
+			}
+		}
+	}, []);
 
 	const [upcomingBusses, setUpcomingBusses] = useState<BusInfo[]>([]);
 	const [allBusses, setAllBusses] = useState<BusInfo[]>([]);
@@ -83,7 +99,7 @@ const App: React.FC = () => {
 			{
 				duration: 55000,
 				position: "bottom-center",
-				className: "bg-neutral-300 text-zinc-900 rounded text-xs"
+				className: "bg-neutral-300 text-zinc-900 border border-neutral-300 rounded-lg text-xs"
 			}
 		);
 	};
@@ -101,10 +117,10 @@ const App: React.FC = () => {
 						setClosestBusStations([]);
 						setToggleLocation(false);
 						notify("Yakında hiç otobüs durağı görünmüyor. Lütfen tekrar deneyiniz.");
+						setIsLoading(false);
 					}
 				}).catch((error) => {
 					notify(`Servis hatası: ${error}`);
-				}).finally(() => {
 					setIsLoading(false);
 				});
 			}, () => {
@@ -137,148 +153,151 @@ const App: React.FC = () => {
 				setAllBusses(response.allBusses);
 			}
 		}
-		).catch((error) => {
-			console.error(error);
+		).catch(() => {
 			notify("Bir hata oluştu. Lütfen daha sonra tekrar deneyin.");
 		}
 		).finally(() => {
 			setLastUpdateTime(new Date().toTimeString().split(" ")[0]);
 			setIsLoading(false);
-
 		});
 
 	};
 
 	return (
-		<div className="container mx-auto max-w-md mt-10 px-4 bg-neutral-950 text-zinc-300">
-			<h1 className="text-2xl font-bold my-5 text-center">Ne Zaman Otobüs 🚌 ⏰</h1>
-			<div className="mb-4">
-				{!toggleLocation ?
-					<input type="text" className="w-full bg-neutral-900 px-3 py-3 rounded appearance-none text-xs text-zinc-300" placeholder="Durak Numarası (Örnek: 30374)" value={busStationInfo.id} onChange={handleInputChange}
-					/> :
-					<select
-						className="w-full bg-neutral-900 px-3 py-3 rounded appearance-none text-xs text-zinc-300"
-						value={busStationInfo.id}
-						onChange={handleSelectChange}
+		<div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 transition-colors duration-300">
+			<div className="container mx-auto max-w-md pt-1 pb-1 px-4 bg-neutral-100 text-zinc-900 dark:bg-neutral-950 dark:text-zinc-300">
+				<h1 className="text-2xl font-bold my-5 text-center">Ne Zaman Otobüs 🚌 ⏰</h1>
+				<div className="mb-4">
+					{!toggleLocation ?
+						<input type="text" className="w-full bg-white text-zinc-900 dark:bg-neutral-900 dark:text-zinc-300 px-3 py-3 rounded-lg appearance-none text-xs" placeholder="Durak Numarası (Örnek: 30374)" value={busStationInfo.id} onChange={handleInputChange}
+						/> :
+						<select
+							className="w-full bg-white text-zinc-900 dark:bg-neutral-900 dark:text-zinc-300 px-3 py-3 rounded-lg appearance-none text-xs"
+							value={busStationInfo.id}
+							onChange={handleSelectChange}
+						>
+							<option value="">Durak Seçin</option>
+							{closestBusStations.map((station) => (
+								<option key={station.id} value={station.id}>
+									{station.name}
+								</option>
+							))}
+						</select>
+					}
+				</div>
+				<div className="flex justify-between items-center">
+					<button
+						className="bg-blue-600 text-white
+					disabled:bg-neutral-200 disabled:text-zinc-400 dark:disabled:text-zinc-600 dark:disabled:bg-neutral-800
+					py-2 px-4 rounded-lg w-full mr-3"
+						onClick={getUpcomingBuses}
+						disabled={isLoading || !validateInput()}
 					>
-						<option value="">Durak Seçin</option>
-						{closestBusStations.map((station) => (
-							<option key={station.id} value={station.id}>
-								{station.name}
-							</option>
-						))}
-					</select>
+						{
+							isLoading ? (
+								<div className="flex items-center justify-center">
+									<span className="mr-2">Yükleniyor</span>
+									<svg
+										className="animate-spin h-5 w-5 "
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+									>
+										<circle
+											className="opacity-25"
+											cx="12"
+											cy="12"
+											r="10"
+											stroke="currentColor"
+											strokeWidth="4"
+										></circle>
+										<path
+											className="opacity-75"
+											fill="currentColor"
+											d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+										></path>
+									</svg>
+								</div>
+							) : (
+								"İstek Gönder"
+							)
+						}
+					</button>
+					<button className={"py-2 px-2 rounded-lg" + (toggleLocation ? " bg-indigo-600 dark:bg-indigo-600" : " bg-neutral-200 dark:bg-neutral-800")}
+						disabled={isLoading}
+						onClick={() => setToggleLocation(!toggleLocation)}
+					>
+						📍
+					</button>
+				</div>
+				{
+					lastUpdateTime !== "" && (upcomingBusses.length > 0 || allBusses.length > 0) && (
+						<div className="text-sm text-green-700 mt-4 text-center">
+							Son Güncelleme Zamanı: {lastUpdateTime}
+						</div>
+					)
 				}
-			</div>
-			<div className="flex justify-between items-center">
-				<button
-					className="bg-blue-900 hover:bg-blue-800 focus:bg-blue-900 disabled:bg-neutral-900 py-2 px-4 rounded w-full mr-3"
-					onClick={getUpcomingBuses}
-					disabled={isLoading || !validateInput()}
-				>
+				<div className="mt-4">
 					{
-						isLoading ? (
-							<div className="flex items-center justify-center">
-								<span className="mr-2">Yükleniyor</span>
-								<svg
-									className="animate-spin h-5 w-5 "
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-								>
-									<circle
-										className="opacity-25"
-										cx="12"
-										cy="12"
-										r="10"
-										stroke="currentColor"
-										strokeWidth="4"
-									></circle>
-									<path
-										className="opacity-75"
-										fill="currentColor"
-										d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-									></path>
-								</svg>
+						upcomingBusses.length > 0 && (
+							<div className="border-t border-neutral-200 dark:border-neutral-800 pt-4">
+
+								<div className="flex justify-between items-baseline">
+									<h2 className="text-lg font-bold mb-4">Yaklaşan Otobüsler</h2>
+									{
+										busStationInfo.name && (
+											<span className="text-sm">{busStationInfo.name}</span>
+										)
+
+									}
+								</div>
+								{upcomingBusses.map((item, index) => (
+									<BusCard key={index} {...item}></BusCard>
+								))}
 							</div>
-						) : (
-							"İstek Gönder"
 						)
 					}
-				</button>
-				<button className={"disabled:bg-neutral-900 py-2 px-2 rounded " + (toggleLocation ? "bg-blue-900 hover:bg-blue-800 focus:bg-blue-900" : "bg-neutral-900 hover:bg-neutral-800 focus:bg-neutral-900")} disabled={isLoading}
-					onClick={() => setToggleLocation(!toggleLocation)}
-				>
-					📍
-				</button>
-			</div>
-			{
-				lastUpdateTime !== "" && (upcomingBusses.length > 0 || allBusses.length > 0) && (
-					<div className="text-sm text-green-700 mt-4 text-center">
-						Son Güncelleme Zamanı: {lastUpdateTime}
-					</div>
-				)
-			}
-			<div className="mt-4">
-				{
-					upcomingBusses.length > 0 && (
-						<div className="border-t border-neutral-900 pt-4">
+					{
+						allBusses.length > 0 && (
+							<div className="border-t border-neutral-200 dark:border-neutral-800 pt-4">
+								<div className="flex justify-between items-baseline">
+									<h2 className="text-lg font-bold mb-4">Tüm Otobüsler</h2>
+									{
+										busStationInfo.name && (
+											<span className="text-sm">{busStationInfo.name}</span>
+										)
 
-							<div className="flex justify-between items-baseline">
-								<h2 className="text-lg font-bold mb-4">Yaklaşan Otobüsler</h2>
-								{
-									busStationInfo.name && (
-										<span className="text-sm">{busStationInfo.name}</span>
-									)
-
+									}
+								</div>					{
+									allBusses.map((item, index) => (
+										<BusCard key={index} {...item}></BusCard>
+									))
 								}
 							</div>
-							{upcomingBusses.map((item, index) => (
-								<BusCard key={index} {...item}></BusCard>
-							))}
-						</div>
-					)
-				}
-				{
-					allBusses.length > 0 && (
-						<div className="border-t border-neutral-900 pt-4">
-							<div className="flex justify-between items-baseline">
-								<h2 className="text-lg font-bold mb-4">Tüm Otobüsler</h2>
-								{
-									busStationInfo.name && (
-										<span className="text-sm">{busStationInfo.name}</span>
-									)
-
-								}
-							</div>					{
-								allBusses.map((item, index) => (
-									<BusCard key={index} {...item}></BusCard>
-								))
-							}
-						</div>
-					)
-				}
-				{
-					(!isLoading && upcomingBusses.length === 0 && allBusses.length === 0 && lastUpdateTime !== "")
-					&& (
-						<>
-							<div className="text-sm text-center text-red-700 mb-4">
-								Veri bulunamadı. Lütfen durak numarasını kontrol edin.
-							</div>
-							<div className="border-t border-neutral-900 pt-4 my-4">
-								<div className="text-xs">
-									Durak numarası genellikle 5 haneli bir sayıdır ve duraklarda bulunan numaralı levhalarda yazmaktadır. Durak numarası bilgisine <a className="text-blue-700" href="https://www.kocaeli.bel.tr/tr/main/hatlar">kocaeli.bel.tr</a> sitesinden de ulaşabilirsiniz.
+						)
+					}
+					{
+						(!isLoading && upcomingBusses.length === 0 && allBusses.length === 0 && lastUpdateTime !== "")
+						&& (
+							<>
+								<div className="text-sm text-center text-red-700 mb-4">
+									Veri bulunamadı. Lütfen durak numarasını kontrol edin.
 								</div>
-							</div>
-						</>
+								<div className="border-t border-neutral-200 dark:border-neutral-800 pt-4 pt-4 my-4">
+									<div className="text-xs">
+										Durak numarası genellikle 5 haneli bir sayıdır ve duraklarda bulunan numaralı levhalarda yazmaktadır. Durak numarası bilgisine <a className="text-blue-700" href="https://www.kocaeli.bel.tr/tr/main/hatlar">kocaeli.bel.tr</a> sitesinden de ulaşabilirsiniz.
+									</div>
+								</div>
+							</>
 
-					)
-				}
+						)
+					}
+				</div>
+				<div className="text-xs border-t border-neutral-200 dark:border-neutral-800 pt-4 pt-4 my-4">
+					Bu uygulama Mustafa Yurdakul tarafından yapılmıştır. Kaynak kodlarına <a className="text-blue-700" href="https://github.com/mustafayurdakul/when-bus">GitHub</a> üzerinden ulaşabilirsiniz.
+				</div>
+				<Toaster />
 			</div>
-			<div className="text-xs border-t border-neutral-900 italic pt-4 my-4">
-				Bu uygulama Mustafa Yurdakul tarafından yapılmıştır. Kaynak kodlarına <a className="text-blue-700" href="https://github.com/mustafayurdakul/when-bus">GitHub</a> üzerinden ulaşabilirsiniz.
-			</div>
-			<Toaster />
 		</div>
 	);
 };
